c3b384ca59789b752dbcd113ff10a1252dd0387b
TazarChriss <77143206+tazarchriss@users.noreply.github.com>
2022-05-04 03:04:12 +0300

    Create uploadimage.php

---
+++ uploadimage.php
@@ -0,0 +1,32 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $file=$_FILES['file'];
+    // print_r($file);
+    $fileName=$_FILES['file']['name'];
+    $fileTmpName=$_FILES['file']['tmp_name'];
+    $fileSize=$_FILES['file']['size'];
+    $fileError=$_FILES['file']['error'];
+
+    $fileExt=explode('.',$fileName);
+    $fileActualExt=strtolower(end($fileExt));
+
+    $allowed=array('jpg','jpeg','png','pdf');
+    if(in_array($fileActualExt,$allowed)){
+        if ($fileError===0) {
+            if ($fileSize<1000000) {
+                $fileNameNew=uniqid('',true).".".$fileActualExt;
+                $fileDestination='uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName,$fileDestination);
+                header("Location:index.php?uploadsuccess");
+            }else {
+                echo "You file is too big !";
+            }
+        }else {
+            echo "There was an error uploading your file !"; 
+        }
+    }else{
+        echo 'you cannot upload files of this type !';
+    }
+}
+?>
